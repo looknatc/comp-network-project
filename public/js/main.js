80ffe8492071b068661efa3e62b53d0ddf598489
Yaninchaya Unjitwattana <yaninchaya152@hotmail.com>
2023-04-10 12:35:15 +0700

    edit ui chatroom

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
-const leaveButton = document.getElementById('leave-btn');
-const roomList = document.getElementById('room');
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+const leaveButton = document.getElementById("leave-btn");
+const roomList = document.getElementById("room");
 
 // const rooms = ['Java','PHP']
 // Get username and room from URL
@@ -19,16 +19,16 @@ const socket = io();
 // });
 
 // Join chatroom
-socket.emit('joinRoom', { username, room });
+socket.emit("joinRoom", { username, room });
 
 // Get room and users
-socket.on('roomUsers', ({ room, users }) => {
+socket.on("roomUsers", ({ room, users }) => {
   outputRoomName(room);
   outputUsers(users);
 });
 
 // Message from server
-socket.on('message', (message) => {
+socket.on("message", (message) => {
   console.log(message);
   outputMessage(message);
 
@@ -37,7 +37,7 @@ socket.on('message', (message) => {
 });
 
 // Message submit
-chatForm.addEventListener('submit', (e) => {
+chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   // Get message text
@@ -46,27 +46,36 @@ chatForm.addEventListener('submit', (e) => {
 
   // msg = msg.trim();
 
-//   if (!msg) {
-//     return false;
-//   }
+  //   if (!msg) {
+  //     return false;
+  //   }
 
   // Emit message to server
-  socket.emit('chatMessage', msg);
-
+  socket.emit("chatMessage", msg);
 
   // Clear input
-  e.target.elements.msg.value = '';
+  e.target.elements.msg.value = "";
   e.target.elements.msg.focus();
 });
 
 // Output message to DOM
 function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p> 
+  const div = document.createElement("div");
+
+  if (message.username === username) {
+    div.classList.add("message-sender");
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p> 
   <p class="text">
     ${message.text}
   </p>`;
+  } else {
+    div.classList.add("message-receiver");
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p> 
+    <p class="text">
+      ${message.text}
+    </p>`;
+  }
+
   // const p = document.createElement('p');
   // p.classList.add('meta');
   // p.innerText = message.username;
@@ -76,10 +85,10 @@ function outputMessage(message) {
   // para.classList.add('text');
   // para.innerText = message.text;
   // div.appendChild(para);
-  document.querySelector('.chat-messages').appendChild(div);
+  document.querySelector(".chat-messages").appendChild(div);
 }
-socket.on("directMessage",(x)=>{
-  console.log("directMessage",x);
+socket.on("directMessage", (x) => {
+  console.log("directMessage", x);
 });
 
 // Add room name to DOM
@@ -90,8 +99,12 @@ function outputRoomName(room) {
 // Add users to DOM
 function outputUsers(users) {
   userList.innerHTML = `
-    ${users.map(user => `<li>${user.username}</li>`).join('')}
-  `
+    ${users
+      .map(
+        (user) => `<li><i class = "fas fa-user-alt"></i> ${user.username}</li>`
+      )
+      .join("")}
+  `;
   // userList.innerHTML = '';
   // users.forEach((user) => {
   //   const li = document.createElement('li');
@@ -101,10 +114,10 @@ function outputUsers(users) {
 }
 
 // //Prompt the user before leave chat room
-leaveButton.addEventListener('click', () => {
-  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+leaveButton.addEventListener("click", () => {
+  const leaveRoom = confirm("Are you sure you want to leave the chatroom?");
   if (leaveRoom) {
-    window.location = '../index.html';
+    window.location = "../index.html";
   } else {
   }
 });
